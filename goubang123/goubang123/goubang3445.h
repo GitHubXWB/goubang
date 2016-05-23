8583c64ee0c53cf81d549efb7ca6304065d6fc39
GitHubXWB <949632230@qq.com>
2016-05-23 12:52:16 +0800

    创建了一个新的文件
    文件名字叫狗帮123

---
+++ goubang123/goubang123/goubang3445.h
@@ -0,0 +1,13 @@
+//
+//  goubang3445.h
+//  goubang123
+//
+//  Created by 肖万邦 on 16/5/23.
+//  Copyright © 2016年 Mr.X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface goubang3445 : UIViewController
+
+@end
---
+++ goubang123/goubang123/goubang3445.m
@@ -0,0 +1,13 @@
+//
+//  goubang3445.m
+//  goubang123
+//
+//  Created by 肖万邦 on 16/5/23.
+//  Copyright © 2016年 Mr.X. All rights reserved.
+//
+
+#import "goubang3445.h"
+
+@implementation goubang3445
+
+@end
