0a98e047804a2a90556fb9cef21b7c1f6950e20a
GitHubXWB <949632230@qq.com>
2016-05-23 12:49:25 +0800

    dbbb
    wuwuwu

---
+++ goubang123/goubang123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  goubang123
+//
+//  Created by 肖万邦 on 16/5/23.
+//  Copyright © 2016年 Mr.X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ goubang123/goubang123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goubang123
+//
+//  Created by 肖万邦 on 16/5/23.
+//  Copyright © 2016年 Mr.X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
